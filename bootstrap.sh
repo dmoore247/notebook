eb6dcb5f0b64a14a1d7d837746d335257e3cd390
Douglas Moore <dmoore247@users.noreply.github.com>
2015-07-19 16:06:56 -0400

    Update bootstrap.sh
    Set correct virtual env stuff.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 # bootstrap.sh
 cd /jupyter
 virtualenv venv
-virtualenv -p /usr/local/bin/python2.7 ...
+virtualenv -p /usr/local/bin/python2.7 venv
+source venv/bin/activate
 cd notebook
 jupyter notebook
